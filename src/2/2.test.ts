29917446d2a1312b0f771dad134e1e16e0d1f449
toby <toby@trint.com>
2020-12-04 10:37:54 +0000

    Day 2 - part one

--- src/2/2.test.ts
+++ src/2/2.test.ts
@@ -2,8 +2,8 @@ import { checkPassword } from './2';
 
 describe('Day 2', () => {
   it('should work for a single password', () => {
-    const input = '17-19 p: pwpzpfbrcpppjppbmppp'; // 9 p's
-    const input2 = '7-9 p: pwpzpfbrcpppjppbmppp'; // 9 p's
+    const input = '17-19 p: pwpzpfbrcpppjppbmppp'; // 11 p's
+    const input2 = '10-11 p: pwpzpfbrcpppjppbmppp'; // 11 p's
     expect(checkPassword(input)).toBe(false);
     expect(checkPassword(input2)).toBe(true);
   });
--- src/2/2.ts
+++ src/2/2.ts
@@ -6,11 +6,10 @@ export const checkPassword = (input: string): boolean => {
     .split('-')
     .map((limit) => Number(limit));
   const letter = letterInput.replace(':', '');
-  const occurrencesOfLetter = password.replace(new RegExp(letter, 'g'), '')
-    .length;
-  return (
-    minimum <= occurrencesOfLetter && occurrencesOfLetter <= maximum
-  );
+  const occurrencesOfLetter = password
+    .split('')
+    .filter((character) => character === letter).length;
+  return minimum <= occurrencesOfLetter && occurrencesOfLetter <= maximum;
 };
 
 const partOne = (input: string[]) => {
